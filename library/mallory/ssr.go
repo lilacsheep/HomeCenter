eacef6d8221b923ab88b1f200e2e731a7f698245
Administrator <shiyang@sky-shining.cn>
2020-12-23 23:18:40 +0800

    future: ssr暂不更新

--- library/mallory/ssr.go
+++ library/mallory/ssr.go
@@ -10,7 +10,7 @@ import (
 )
 
 func init() {
-
+	// ssr 暂时不添加，被封的概率很高，还是暂时不支持比较好
 }
 
 type SSRClient struct {
@@ -22,6 +22,19 @@ type SSRClient struct {
 	l      sync.RWMutex
 	Status bool
 	Cancel context.CancelFunc
+}
+
+func (self *SSRClient) Connect(w http.ResponseWriter, r *http.Request) {
+	self.Direct.Connect(w, r)
+}
+
+func (self *SSRClient) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	self.Direct.ServeHTTP(w, r)
+}
+
+func (self *SSRClient) Stop() error {
+	self.Cancel()
+	return nil
 }
 
 func (self *SSRClient) Init () (err error) {
