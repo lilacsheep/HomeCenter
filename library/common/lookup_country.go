d4ffc165e8bcb35db91e8ce9bef251ab84e63c65
Administrator <admin@example.com>
2020-12-28 14:33:26 +0800

    fix: 获取ip地址错误

--- library/common/lookup_country.go
+++ library/common/lookup_country.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gogf/gf/encoding/gjson"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
-	"github.com/gogf/gf/os/glog"
 )
 
 func LookupCountry(addr string) (string, error) {
