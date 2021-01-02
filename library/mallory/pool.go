170c38e06ad2c31f3a3f5f4456894c986b62a772
Administrator <admin@example.com>
2021-01-03 01:18:46 +0800

    add: dns Balance

--- library/mallory/pool.go
+++ library/mallory/pool.go
@@ -1,9 +1,15 @@
 package mallory
 
 import (
+	"context"
 	"errors"
-	"github.com/gogf/gf/container/gmap"
+	"homeproxy/library/common"
 	"math/rand"
+	"net"
+	"time"
+
+	"github.com/gogf/gf/container/gmap"
+	"github.com/gogf/gf/os/gcache"
 )
 
 var (
@@ -11,12 +17,12 @@ var (
 )
 
 type Balance interface {
-	DoBalance(instances *gmap.TreeMap) (interface{}, error)
+	DoBalance(instances *gmap.TreeMap, domain ...string) (interface{}, error)
 }
 
 type RandomBalance struct{}
 
-func (self *RandomBalance) DoBalance(instances *gmap.TreeMap) (interface{}, error) {
+func (self *RandomBalance) DoBalance(instances *gmap.TreeMap, domain ...string) (interface{}, error) {
 	if instances.IsEmpty() {
 		return nil, ErrNoInstances
 	} else if instances.Size() == 1 {
@@ -31,7 +37,7 @@ type RoundRobinBalance struct {
 	curIndex int
 }
 
-func (self *RoundRobinBalance) DoBalance(instances *gmap.TreeMap) (interface{}, error) {
+func (self *RoundRobinBalance) DoBalance(instances *gmap.TreeMap, domain ...string) (interface{}, error) {
 	if instances.IsEmpty() {
 		return nil, ErrNoInstances
 	} else if instances.Size() == 1 {
@@ -42,6 +48,44 @@ func (self *RoundRobinBalance) DoBalance(instances *gmap.TreeMap) (interface{}, 
 		}
 		self.curIndex++
 		return instances.Values()[self.curIndex], nil
+	}
+}
+
+// dns 路由负载
+type DNSBalance struct {
+	dns   *net.Resolver
+	cache *gcache.Cache
+}
+
+func (self *DNSBalance) DoBalance(instances *gmap.TreeMap, domains ...string) (interface{}, error) {
+	var (
+		code string
+	)
+	if instances.IsEmpty() {
+		return nil, ErrNoInstances
+	} else if instances.Size() == 1 {
+		return instances.Values()[0], nil
+	} else {
+		domain := domains[0]
+		if v, _ := self.cache.Get(domain); v == nil {
+			addrs, _ := self.dns.LookupHost(context.Background(), domain)
+			code, _ = common.LookupCountry(addrs[0])
+			self.cache.Set(domain, code, time.Minute * 10)
+		} else {
+			code = v.(string)
+		}
+		instances.Iterator(func(key, value interface{}) bool {
+			return true
+		})
+
+		return nil, nil
+	}
+}
+
+func NewDNSBalance(dns *net.Resolver) *DNSBalance {
+	return &DNSBalance{
+		dns: dns,
+		cache: gcache.New(600),
 	}
 }
 
