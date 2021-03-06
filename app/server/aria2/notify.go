01b13d3db91d58054152af1fa84441587950cad8
lilacsheep <lilacsheep@hotmail.com>
2021-03-07 00:40:53 +0800

    add: download over clean empty dir

--- app/server/aria2/notify.go
+++ app/server/aria2/notify.go
@@ -3,7 +3,9 @@ package aria2
 import (
 	"homeproxy/app/models"
 	"homeproxy/library/filedb2"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"github.com/gogf/gf/os/glog"
 	"github.com/gogf/gf/util/gconv"
@@ -28,22 +30,54 @@ func (CustomNotify) OnDownloadComplete(events []rpc.Event)   {
 		return
 	}
 	cleanSize = settings.AutoClean * 1048576
+	
 	for _, event := range events {
-		infos, err := server.GetFiles(event.Gid)
+		taskInfo, err := server.TellStatus(event.Gid)
 		if err != nil {
 			glog.Errorf("get task %s file error: %s", event.Gid, err.Error())
 		} else {
-			if len(infos) > 1 {
-				for _, info := range infos {
-					if gconv.Int(info.Length) < cleanSize {
-						glog.Infof("clean file %s size is %s b need %d b", info.Path, gconv.Int(info.Length), cleanSize)
-						os.RemoveAll(info.Path)
+			if taskInfo.BitTorrent.Info.Name != "" {
+				if len(taskInfo.Files) > 1 {
+					for _, info := range taskInfo.Files {
+						if gconv.Int(info.Length) < cleanSize {
+							glog.Infof("clean file %s size is %s b need %d b", info.Path, gconv.Int(info.Length), cleanSize)
+							os.RemoveAll(info.Path)
+						}
+					}
+				}
+				dirPath := filepath.Join(taskInfo.Dir, taskInfo.BitTorrent.Info.Name)
+				dirs, err := getDirList(dirPath)
+				if err != nil {
+					glog.Errorf("get dir path error: %s", err.Error())
+				} else {
+					for _, d := range dirs {
+						t, _ := ioutil.ReadDir(d)
+						if len(t) == 0 {
+							os.RemoveAll(d)
+						}
 					}
 				}
 			}
 		}
 	}
-
 }
 func (CustomNotify) OnDownloadError(events []rpc.Event)      {}
 func (CustomNotify) OnBtDownloadComplete(events []rpc.Event) {}
+
+
+func getDirList(dirpath string) ([]string, error) {
+	var dirs []string
+	err := filepath.Walk(dirpath,
+		func(path string, f os.FileInfo, err error) error {
+			if f == nil {
+				return err
+			}
+			if f.IsDir() {
+				dirs = append(dirs, path)
+				return nil
+			}
+
+			return nil
+		})
+	return dirs, err
+}
