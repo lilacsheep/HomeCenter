d90ccee8646d4d97cc90d8afe66f011963ff54dc
lilacsheep <lilacsheep@hotmail.com>
2021-03-07 02:38:57 +0800

    fix: download error

--- app/api/proxy_download_api.go
+++ app/api/proxy_download_api.go
@@ -83,7 +83,7 @@ func (self *ProxyDownloadApi) Download(r *ghttp.Request) {
 	} else {
 
 		info := models.DownloadFileList{}
-		err := filedb2.DB.One("vkey", vkey, &info)
+		err := filedb2.DB.One("Vkey", vkey, &info)
 		if err != nil {
 			if err == storm.ErrNotFound {
 				r.Response.WriteStatus(http.StatusNotFound)
