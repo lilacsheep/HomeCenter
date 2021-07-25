5b4bc3aeab393233a8b1dfbe94b27287a3455778
lilacsheep <lilacsheep@hotmail.com>
2021-07-25 15:48:58 +0800

    feat: 新增通过geoip获取ip归属地

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/grokify/html-strip-tags-go v0.0.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/nranchev/go-libGeoIP v0.0.0-20170629073846-d6d4a9a4c7e8 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/shirou/gopsutil v3.21.1+incompatible
@@ -21,9 +22,9 @@ require (
 	github.com/zyxar/argo v0.0.0-20201218032338-28f97c7a4b97
 	go.etcd.io/bbolt v1.3.5 // indirect
 	go.opentelemetry.io/otel v0.17.0 // indirect
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
-	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
-	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
 	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
@@ -31,6 +32,6 @@ require (
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/fsnotify.v1 v1.4.7
-	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/ini.v1 v1.62.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- library/common/lookup_country.go
+++ library/common/lookup_country.go
@@ -1,19 +1,66 @@
 package common
 
 import (
+	"errors"
 	"regexp"
 	"strings"
 
 	"github.com/gogf/gf/encoding/gjson"
 	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
+	"github.com/gogf/gf/os/gfile"
+	libgeo "github.com/nranchev/go-libGeoIP"
 )
 
 const (
-	apiUrl string = "http://api.dukeshi.cn:29090/api/location"
+	apiUrl     string = "http://api.dukeshi.cn:29090/api/location"
+	geoDataUrl string = "https://raw.githubusercontent.com/Loyalsoldier/geoip/release/geoip.dat"
+	dataFile   string = "data/geoip.dat"
 )
 
-func LookupCountry(addr string) (string, error) {
+var (
+	gi *libgeo.GeoIP
+	GeoFileNotFoundErr = errors.New("")
+)
+
+func LookupByGeo(addr string) (*libgeo.Location, error) {
+	var err error
+	if !gfile.Exists(dataFile) {
+		return nil, GeoFileNotFoundErr
+	}
+	if gi == nil {
+		gi, err = libgeo.Load(dataFile)
+		if err != nil {
+			return nil, err
+		}
+	}
+	ip := gi.GetLocationByIP(addr)
+	return ip, err
+}
+
+func InitGeoFile(proxyUrl ...string) error {
+	cli := ghttp.NewClient()
+	if len(proxyUrl) > 0 {
+		cli.SetProxy(proxyUrl[0])
+	}
+	resp, err := cli.Get(geoDataUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Close()
+
+	err = gfile.PutBytes("data/geoip.dat", resp.ReadAll())
+	if err != nil {
+		return err
+	}
+	gi, err = libgeo.Load(dataFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LookupByApi(addr string) (string, error) {
 	client := ghttp.NewClient()
 	resp, err := client.Get(apiUrl, g.Map{"ip": addr})
 	if err != nil {
@@ -23,6 +70,19 @@ func LookupCountry(addr string) (string, error) {
 	data := resp.ReadAllString()
 	j := gjson.New(data)
 	return j.GetString("country"), nil
+}
+
+func LookupCountry(addr string) (string, error) {
+	location, err := LookupByGeo(addr)
+	if err != nil {
+		if err == GeoFileNotFoundErr {
+			go InitGeoFile()
+			return LookupByApi(addr)
+		} else {
+			return "", err
+		}
+	}
+	return location.CountryCode, nil
 }
 
 func CheckIp(ip string) bool {
