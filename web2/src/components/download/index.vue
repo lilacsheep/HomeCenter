172ef6aa4b10c50504b887f5b0eedcefeefc5fbb
lilacsheep <lilacsheep@hotmail.com>
2021-09-03 00:45:48 +0800

    feat: element-ui替换

--- web2/src/components/download/index.vue
+++ web2/src/components/download/index.vue
@@ -6,102 +6,98 @@
         文件下载
       </a-breadcrumb-item>
     </a-breadcrumb>
-    <a-row :gutter="16" style="background: #fbfbfb;border: 1px solid #f4f4f4;height: 100%">
+    <a-row :gutter="16" style="border: 1px solid #f4f4f4;height: 100%">
       <a-col>
-        <a-tabs default-active-key="1" @change="tabClick">
+        <a-tabs default-active-key="1" @change="tabClick" tab-position="left">
           <a-tab-pane key="1" tab="下载列表">
             <a-button-group>
-              <a-button size="mini" type="primary" icon="edit" @click="download.create.visit = true">创建下载</a-button>
-              <a-button size="mini" type="primary" icon="upload" @click="torrent.sync = true">上传种子</a-button>
+              <a-button icon="edit" @click="download.create.visit = true">创建下载</a-button>
+              <a-upload name="file" :multiple="false" accept=".torrent" action="/api/download/torrent">
+                <a-button icon="upload">上传种子</a-button>
+              </a-upload>
             </a-button-group>
-          
-          <!-- <a-input size="mini" placeholder="请输入内容" v-model="roles.filter" style="width: 250px;float: right;">
-            <el-button slot="append" icon="el-icon-search"></el-button>
-          </a-input> -->
-          <span style="float: right;">
-            <a-radio-group v-model="task.query.status" size="mini">
-              <a-radio-button size="small" value="全部">全部</a-radio-button>
-              <a-radio-button size="small" value="下载中">下载中</a-radio-button>
-              <a-radio-button size="small" value="已完成">已完成</a-radio-button>
-              <a-radio-button size="small" value="其他">其他</a-radio-button>
-            </a-radio-group>
-            <a-tag color="green" size="small"><i class="arrow-up"></i>{{global.upload | diskSize}}/秒</a-tag>
-            <a-tag color="orange" size="small"><i class="arrow-down"></i>{{global.download | diskSize}}/秒</a-tag>
-          </span>
-          <a-table :data="download.tasks" stripe size="mini" style="margin-top: 10px;">
-            <a-table-column prop="gid" label="文件名">
-              <template slot-scope="scope">
-                <el-button size="mini" type="text" @click="taskInfoOpen(scope.row)">{{getTaskName(scope.row)}}</el-button>
-              </template>
-            </a-table-column>
-            <a-table-column prop="totalLength" label="大小" width="100">
-              <template slot-scope="scope">
-                {{scope.row.totalLength | diskSize}}
-              </template>
-            </a-table-column>
-            <a-table-column prop="status" label="状态" width="100">
-              <template slot-scope="scope">
-                <span v-if="scope.row.status == 'active'">{{(scope.row.completedLength / scope.row.totalLength * 100).toFixed(2)}}%</span>
-                <span v-else-if="scope.row.status == 'stopped'">已停止</span>
-                <span v-else-if="scope.row.status == 'paused'">已暂停</span>
-                <span v-else-if="scope.row.status == 'complete'">已完成</span>
-                <span v-else>{{scope.row.status}}</span>
-              </template>
-            </a-table-column>
+            <span style="float: right;">
+              <a-radio-group v-model="task.query.status" size="small">
+                <a-radio-button value="全部">全部</a-radio-button>
+                <a-radio-button value="下载中">下载中</a-radio-button>
+                <a-radio-button value="已完成">已完成</a-radio-button>
+                <a-radio-button value="其他">其他</a-radio-button>
+              </a-radio-group>
+              <a-tag color="green" size="small"><i class="arrow-up"></i>{{global.upload | diskSize}}/秒</a-tag>
+              <a-tag color="orange" size="small"><i class="arrow-down"></i>{{global.download | diskSize}}/秒</a-tag>
+            </span>
+            <a-table :data="download.tasks" stripe size="mini" style="margin-top: 10px;">
+              <a-table-column prop="gid" label="文件名">
+                <template slot-scope="scope">
+                  <el-button size="mini" type="text" @click="taskInfoOpen(scope.row)">{{getTaskName(scope.row)}}</el-button>
+                </template>
+              </a-table-column>
+              <a-table-column prop="totalLength" label="大小" width="100">
+                <template slot-scope="scope">
+                  {{scope.row.totalLength | diskSize}}
+                </template>
+              </a-table-column>
+              <a-table-column prop="status" label="状态" width="100">
+                <template slot-scope="scope">
+                  <span v-if="scope.row.status == 'active'">{{(scope.row.completedLength / scope.row.totalLength * 100).toFixed(2)}}%</span>
+                  <span v-else-if="scope.row.status == 'stopped'">已停止</span>
+                  <span v-else-if="scope.row.status == 'paused'">已暂停</span>
+                  <span v-else-if="scope.row.status == 'complete'">已完成</span>
+                  <span v-else>{{scope.row.status}}</span>
+                </template>
+              </a-table-column>
 
-            <a-table-column prop="downloadSpeed" label="下载" width="100">
-              <template slot-scope="scope">
-                {{scope.row.downloadSpeed | diskSize}}/秒
-              </template>
-            </a-table-column>
-            <a-table-column prop="uploadSpeed" label="上传" width="100">
-              <template slot-scope="scope">
-                {{scope.row.uploadSpeed | diskSize}}/秒
-              </template>
-            </a-table-column>
-            <a-table-column label="操作" fixed="right" width="100">
-              <template slot-scope="scope">
-                <el-popconfirm v-if="scope.row.status == 'paused'" title="是否继续该任务？" @onConfirm="start_task(scope.row)">
-                  <el-button slot="reference" style="color: green" type="text" size="mini" icon="el-icon-caret-right"></el-button>
-                </el-popconfirm>
-                <el-popconfirm v-if="scope.row.status == 'error'" title="是否继续该任务？" @onConfirm="start_task(scope.row)">
-                  <el-button slot="reference" style="color: green" type="text" size="mini" icon="el-icon-caret-right"></el-button>
-                </el-popconfirm>
-                <el-popconfirm v-if="scope.row.status == 'active'" title="是否暂停该任务？" @onConfirm="cancel_task(scope.row)">
-                  <el-button slot="reference" style="color: red" type="text" size="mini" icon="el-icon-switch-button"></el-button>
-                </el-popconfirm>
-                <el-popconfirm title="是否删除该任务？" @onConfirm="remove_task(scope.row)">
-                  <el-button slot="reference" style="color: red" type="text" size="mini" icon="el-icon-delete"></el-button>
-                </el-popconfirm>
-              </template>
-            </a-table-column>
-          </a-table>
+              <a-table-column prop="downloadSpeed" label="下载" width="100">
+                <template slot-scope="scope">
+                  {{scope.row.downloadSpeed | diskSize}}/秒
+                </template>
+              </a-table-column>
+              <a-table-column prop="uploadSpeed" label="上传" width="100">
+                <template slot-scope="scope">
+                  {{scope.row.uploadSpeed | diskSize}}/秒
+                </template>
+              </a-table-column>
+              <a-table-column label="操作" fixed="right" width="100">
+                <template slot-scope="scope">
+                  <el-popconfirm v-if="scope.row.status == 'paused'" title="是否继续该任务？" @onConfirm="start_task(scope.row)">
+                    <el-button slot="reference" style="color: green" type="text" size="mini" icon="el-icon-caret-right"></el-button>
+                  </el-popconfirm>
+                  <el-popconfirm v-if="scope.row.status == 'error'" title="是否继续该任务？" @onConfirm="start_task(scope.row)">
+                    <el-button slot="reference" style="color: green" type="text" size="mini" icon="el-icon-caret-right"></el-button>
+                  </el-popconfirm>
+                  <el-popconfirm v-if="scope.row.status == 'active'" title="是否暂停该任务？" @onConfirm="cancel_task(scope.row)">
+                    <el-button slot="reference" style="color: red" type="text" size="mini" icon="el-icon-switch-button"></el-button>
+                  </el-popconfirm>
+                  <el-popconfirm title="是否删除该任务？" @onConfirm="remove_task(scope.row)">
+                    <el-button slot="reference" style="color: red" type="text" size="mini" icon="el-icon-delete"></el-button>
+                  </el-popconfirm>
+                </template>
+              </a-table-column>
+            </a-table>
           </a-tab-pane>
           <a-tab-pane key="2" tab="配置管理">
-            <a-card style="padding: 5px">
+            <a-card style="padding: 0">
               <a-form-model :model="settings.form" :label-col="labelCol" :wrapper-col="wrapperCol">
-                <a-form-model-item label="地址" label-width="100px">
-                  <a-input size="small" v-model="settings.form.aria2_url" style="width: 220px"></a-input>
+                <a-form-model-item label="地址">
+                  <a-input v-model="settings.form.aria2_url" style="width: 220px"></a-input>
                 </a-form-model-item>
-                <a-form-model-item label="Token" label-width="100px">
-                  <a-input size="small" v-model="settings.form.aria2_token" style="width: 220px"></a-input>
+                <a-form-model-item label="Token">
+                  <a-input v-model="settings.form.aria2_token" style="width: 220px"></a-input>
                 </a-form-model-item>
-                <a-form-model-item label="清理" label-width="100px">
-                  <a-input v-model="settings.form.auto_clean" style="width: 220px" size="small">
-                    <template slot="append">MB</template>
-                  </a-input>
+                <a-form-model-item label="清理">
+                  <a-input v-model="settings.form.auto_clean" style="width: 220px" addon-after="MB"></a-input>
                   <span style="color: #909399;font-size: 11px">自动清理BT文件夹内不满足文件大小的文件, 0为关闭</span>
                 </a-form-model-item>
                 <a-form-model-item label="自动同步">
-                  <el-select size="small" v-model="settings.form.auto_update_bt_tracker" placeholder="请选择">
-                    <a-select-option label="关闭" value=""></a-select-option>
-                    <a-select-option label="每小时" value="@hourly"></a-select-option>
-                    <a-select-option label="每天" value="@every 24h"></a-select-option>
-                  </el-select>
+                  <a-select v-model="settings.form.auto_update_bt_tracker" placeholder="请选择" style="width: 220px">
+                    <a-select-option value="">关闭</a-select-option>
+                    <a-select-option value="@hourly">每小时</a-select-option>
+                    <a-select-option value="@every 24h">每天</a-select-option>
+                  </a-select>
                   <span style="color: #909399;font-size: 11px">自动同步最新的BT服务器</span>
                 </a-form-model-item>
                 <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
-                  <a-button size="small" type="primary"  @click="submit_update_settings">立即更新</a-button>
+                  <a-button type="primary"  @click="submit_update_settings">立即更新</a-button>
                 </a-form-model-item>
               </a-form-model>
             </a-card>
@@ -111,68 +107,62 @@
             </a-table>
           </a-tab-pane>
         </a-tabs>
-        <el-dialog title="新建下载" :visible.sync="download.create.visit">
-          <el-form :model="download.create.form" label-position="right">
-            <a-form-model-item label="地址" label-width="100px">
-              <a-input size="small" type="textarea" v-model="download.create.form.url" :rows="3"></a-input>
-            </a-form-model-item>
-          </el-form>
-          <div slot="footer" class="dialog-footer">
-            <el-button size="small" @click="download.create.visit = false">取 消</el-button>
-            <el-button size="small" type="primary" @click="submit_create_task">确 定</el-button>
-          </div>
-        </el-dialog>
-        <el-dialog custom-class="upload_diglog" :title="种子上传" :visible.sync="torrent.sync"  width="380px">
-          <el-upload class="upload-demo" drag action="/api/download/torrent">
-            <i class="el-icon-upload"></i>
-            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-          </el-upload>
-        </el-dialog>
-        <el-drawer :withHeader="false" :visible.sync="task.visible" :before-close="taskInfoClose" size="60%">
-          <table style="border: 1px solid #f2f2f2" width="100%">
-            <tr>
-              <td width="100px" style="background-color: #f2f2f2;padding: 0">文件名</td>
-              <td colspan="3">{{task.info.filename}}</td>
-            </tr>
-            <tr>
-              <td width="100px" style="background-color: #f2f2f2;padding: 0">GID</td>
-              <td colspan="3">{{task.info.status.gid}}</td>
-            </tr>
-            <tr>
-              <td width="100px" style="background-color: #f2f2f2;padding: 0">错误</td>
-              <td colspan="3">{{task.info.status.errorMessage}}</td>
-            </tr>
-            <tr>
-              <td width="100px" style="background-color: #f2f2f2;padding: 0">大小</td>
-              <td>{{task.info.status.totalLength | diskSize}}</td>
-              <td width="100px" style="background-color: #f2f2f2;padding: 0">状态</td>
-              <td>{{task.info.status.status}}</td>
-            </tr>
-            <tr>
-              <td width="100px" style="background-color: #f2f2f2;padding: 0">已上传</td>
-              <td>{{task.info.status.uploadLength | diskSize}}</td>
-              <td width="100px" style="background-color: #f2f2f2;padding: 0">已完成</td>
-              <td>{{task.info.status.completedLength | diskSize}}</td>
-            </tr>
-          </table>
-          <a-table :data="task.info.status.files" stripe size="mini" style="margin-top: 10px;" max-height="500">
-            <a-table-column prop="path" label="文件">
-              <template slot-scope="scope">
-                {{scope.row.path.split("/").slice(-1)[0]}}
-              </template>
-            </a-table-column>
-            <a-table-column prop="completedLength" label="进度" width="100">
-              <template slot-scope="scope">{{(scope.row.completedLength / scope.row.length * 100).toFixed(2)}}%</template>
-              </a-table-column>
-            <a-table-column prop="length" label="大小" width="120">
-              <template slot-scope="scope">
-                {{scope.row.length | diskSize}}
-              </template>
-            </a-table-column>
-          </a-table>
-        </el-drawer>
       </a-col>
     </a-row>
+    <a-modal title="新建下载" v-model="download.create.visit">
+      <template slot="footer">
+        <a-button @click="download.create.visit = false">取 消</a-button>
+        <a-button type="primary" @click="submit_create_task">确 定</a-button>
+      </template>
+      <a-form-model :model="download.create.form" :label-col="labelCol" :wrapper-col="wrapperCol" label-position="right">
+        <a-form-model-item label="地址">
+          <a-input type="textarea" v-model="download.create.form.url" :rows="3"></a-input>
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
+    <a-drawer :withHeader="false" :visible.sync="task.visible" :before-close="taskInfoClose" size="60%">
+      <table style="border: 1px solid #f2f2f2" width="100%">
+        <tr>
+          <td width="100px" style="background-color: #f2f2f2;padding: 0">文件名</td>
+          <td colspan="3">{{task.info.filename}}</td>
+        </tr>
+        <tr>
+          <td width="100px" style="background-color: #f2f2f2;padding: 0">GID</td>
+          <td colspan="3">{{task.info.status.gid}}</td>
+        </tr>
+        <tr>
+          <td width="100px" style="background-color: #f2f2f2;padding: 0">错误</td>
+          <td colspan="3">{{task.info.status.errorMessage}}</td>
+        </tr>
+        <tr>
+          <td width="100px" style="background-color: #f2f2f2;padding: 0">大小</td>
+          <td>{{task.info.status.totalLength | diskSize}}</td>
+          <td width="100px" style="background-color: #f2f2f2;padding: 0">状态</td>
+          <td>{{task.info.status.status}}</td>
+        </tr>
+        <tr>
+          <td width="100px" style="background-color: #f2f2f2;padding: 0">已上传</td>
+          <td>{{task.info.status.uploadLength | diskSize}}</td>
+          <td width="100px" style="background-color: #f2f2f2;padding: 0">已完成</td>
+          <td>{{task.info.status.completedLength | diskSize}}</td>
+        </tr>
+      </table>
+      <a-table :data="task.info.status.files" stripe size="mini" style="margin-top: 10px;" max-height="500">
+        <a-table-column prop="path" label="文件">
+          <template slot-scope="scope">
+            {{scope.row.path.split("/").slice(-1)[0]}}
+          </template>
+        </a-table-column>
+        <a-table-column prop="completedLength" label="进度" width="100">
+          <template slot-scope="scope">{{(scope.row.completedLength / scope.row.length * 100).toFixed(2)}}%</template>
+          </a-table-column>
+        <a-table-column prop="length" label="大小" width="120">
+          <template slot-scope="scope">
+            {{scope.row.length | diskSize}}
+          </template>
+        </a-table-column>
+      </a-table>
+    </a-drawer>
   </a-layout-content>
 </template>
 
@@ -184,8 +174,8 @@ export default {
       torrent: {
         sync: false
       },
-      labelCol: { span: 4 },
-      wrapperCol: { span: 14 },
+      labelCol: { span: 2 },
+      wrapperCol: { span: 16 },
       aria2: {
         options: [],
         columns: [
@@ -317,17 +307,17 @@ export default {
         that.aria2.options = response.detail
       })
     },
-    tabClick: function (tab, event) {
-      if (tab.index === '0') {
+    tabClick: function (key) {
+      if (key === '1') {
         if (!this.timer) {
           this.timer = setInterval(this.refresh_tasks, 1000)
         }
-      } else if (tab.index === '1') {
+      } else if (key === '2') {
         if (this.timer) {
           clearInterval(this.timer)
           this.timer = undefined
         } 
-      } else if (tab.index === '2') {
+      } else if (key === '3') {
         if (this.timer) {
           clearInterval(this.timer)
           this.timer = undefined
