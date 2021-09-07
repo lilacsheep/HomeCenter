10fac2a8849f30403651c0d1a6fcfaf2768345ed
lilacsheep <lilacsheep@hotmail.com>
2021-09-07 10:55:34 +0800

    feat: docker相关

--- app/services/requests/docker.go
+++ app/services/requests/docker.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"homeproxy/library/docker"
 
-	"github.com/gogf/gf/net/ghttp"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
+	"github.com/gogf/gf/net/ghttp"
 )
 
 // CreateContainerRequest 容器创建请求
@@ -23,4 +24,32 @@ func (req *CreateContainerRequest) Exec(r *ghttp.Request) (response MessageRespo
 		return *response.SystemError(err)
 	}
 	return *response.SuccessWithDetail(body.ID)
+}
+
+// ContainersListRequest 容器清单
+type ContainersListRequest struct {
+	types.ContainerListOptions
+}
+
+// Exec 执行
+func (req *ContainersListRequest) Exec(r *ghttp.Request) MessageResponse {
+	response := MessageResponse{}
+	containers, err := docker.Docker.ContainerList(context.Background(), req.ContainerListOptions)
+	if err != nil {
+		return *response.SystemError(err)
+	}
+	return *response.SuccessWithDetail(containers)
+}
+
+type ListImagesRequest struct {
+	types.ImageListOptions
+}
+
+func (req ListImagesRequest) Exec(r *ghttp.Request) MessageResponse {
+	response := MessageResponse{}
+	images, err := docker.Docker.ImageList(context.Background(), req.ImageListOptions)
+	if err != nil {
+		return *response.SystemError(err)
+	}
+	return *response.SuccessWithDetail(images)
 }
