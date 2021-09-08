20c3173f86d5e04433f7788820034e1f464d840b
lilacsheep <lilacsheep@hotmail.com>
2021-09-08 14:38:05 +0800

    css

--- web/src/components/login/login.vue
+++ web/src/components/login/login.vue
@@ -1,29 +1,27 @@
 <template>
   <a-layout id="components-layout-demo-custom-trigger" :style="{backgroundImage: `url(${backgroundImage})`}">
-    <a-layout>      
-      <a-layout-content :style="{ margin: '8px', padding: '12px', minHeight: '280px', 'text-align': 'center'}">
-          <img :src="imgUrl" class="logo" style="height: 150px;"/>
-          <a-row :gutter="16" style="margin: 0 auto; text-align:center;  background: #fff;width:400px; padding: 10px;border: 1px solid #f2f2f2">
-              <a-form-model :model="formInline" @submit="handleSubmit" @submit.native.prevent style="">
-                <a-form-model-item>
-                <a-input v-model="formInline.username" placeholder="用户名">
-                    <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
-                </a-input>
-                </a-form-model-item>
-                <a-form-model-item>
-                <a-input v-model="formInline.password" type="password" placeholder="密码">
-                    <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
-                </a-input>
-                </a-form-model-item>
-                <a-form-model-item>
-                <a-button style="width: 100px" type="primary" html-type="submit" :disabled="formInline.username === '' || formInline.password === ''">
-                    登陆
-                </a-button>
-                </a-form-model-item>
-            </a-form-model>
-          </a-row>
-      </a-layout-content>
-    </a-layout>
+    <a-layout-content :style="{ margin: '8px', padding: '12px', minHeight: '280px', 'text-align': 'center'}">
+        <img :src="imgUrl" class="logo" style="height: 150px;"/>
+        <a-row :gutter="16" style="margin: 0 auto; text-align:center;  background: #fff;width:400px; padding: 10px;border: 1px solid #f2f2f2">
+            <a-form-model :model="formInline" @submit="handleSubmit" @submit.native.prevent style="">
+              <a-form-model-item>
+              <a-input v-model="formInline.username" placeholder="用户名">
+                  <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
+              </a-input>
+              </a-form-model-item>
+              <a-form-model-item>
+              <a-input v-model="formInline.password" type="password" placeholder="密码">
+                  <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
+              </a-input>
+              </a-form-model-item>
+              <a-form-model-item>
+              <a-button style="width: 100px" type="primary" html-type="submit" :disabled="formInline.username === '' || formInline.password === ''">
+                  登陆
+              </a-button>
+              </a-form-model-item>
+          </a-form-model>
+        </a-row>
+    </a-layout-content>
   </a-layout>
 </template>
 <script>
