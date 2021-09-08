655116a0b5c7ee6c6f956b967827491896f4c216
lilacsheep <lilacsheep@hotmail.com>
2021-09-08 23:37:29 +0800

    css

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,13 +1,11 @@
 import Router from 'vue-router'
 import Herader from '../components/header.vue'
 import Dashboard from '../components/website/index.vue'
-import AgentIndex from '../components/agent/index.vue'
 import Login from '../components/login/login.vue'
 import Download from '../components/download/index.vue'
 import Users from '../components/user/index.vue'
 import DDNS from '../components/ddns/index.vue'
 import Containers from '../components/docker/containers.vue'
-import Webssh from '../components/webssh/index.vue'
 import Vue from 'vue'
 
 
@@ -51,12 +49,6 @@ const router = new Router({
             path: '/containers',
             name: 'containers',
             component: Containers,
-            props: true
-          },
-          {
-            path: '/webssh',
-            name: 'webssh',
-            component: Webssh,
             props: true
           },
         ],
